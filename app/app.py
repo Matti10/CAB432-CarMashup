dbb9ac06e1cb2de4ad9209db3aa87505e4111ac6
Matti10 <mattias.winsen@outlook.com.au>
2022-09-09 15:29:58 +1000

    more work

--- app/app.py
+++ app/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SECRET_KEY'] = "thisIsASecret;)" #config secret key fpr WTF
+
+Bootstrap(app) #init Flask Bootstrap
+
+#config forms
+class SelectionForm(FlaskForm):
+    name = StringField("Please pick a car!",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+#get car data
+
+
+@app.route("/",methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html',name="Lorem Ipsum")
+    form = SelectionForm()
+    name = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data  = "" #reset the form
+
+
+    return render_template('index.html',name=name,form=form)
 
 @app.route("/hello/")
 @app.route("/hello/<var>")
