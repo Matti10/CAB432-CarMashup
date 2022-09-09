b0676d2245a7c261caf872d1660018a811a55283
Matti10 <mattias.winsen@outlook.com.au>
2022-09-09 16:42:53 +1000

    more work

--- app/app.py
+++ app/app.py
@@ -3,6 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import requests
+from serpapi import GoogleSearch
+import os, json
+
+serpKey = '2d71a3fcd8c12e9ec250ddb9dd9743f008a2f71d795430ea67fb3aaa7d0b2247'
+ninjaKey = 'k97Uta4mX0lUYEmNVdUDEg==c8U5pnVM77PBhcXP'
 
 app = Flask(__name__)
 
@@ -12,27 +18,50 @@ Bootstrap(app) #init Flask Bootstrap
 
 #config forms
 class SelectionForm(FlaskForm):
-    name = StringField("Please pick a car!",validators=[DataRequired()])
+    carModel = StringField("Enter a Car model!",validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-#get car data
 
 
 @app.route("/",methods = ['POST', 'GET'])
 def index():
     form = SelectionForm()
-    name = ""
+    model = ""
+    carData = ""
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data  = "" #reset the form
+        model = form.carModel.data
+        form.carModel.data  = "" #reset the form
+        
+        #get model data
+        api_url = 'https://api.api-ninjas.com/v1/cars?model={}'.format(model)
+        response = requests.get(api_url, headers={'X-Api-Key': ninjaKey})
+    
 
+        carData = response.text
 
-    return render_template('index.html',name=name,form=form)
+        if carData == []:
+            carData = "Unfortunaterly, your car is too rare (and cool) for us to get info on it!! Enjoy the below pictures"
+
+        #get images
+        params = {
+            "api_key": serpKey,
+            "engine": "google",
+            "q": model + "car",
+            "tbm": "isch"
+        }
+
+        # search = GoogleSearch(params)
+        # results = search.get_dict()
+
+        # images = results
+        # images = (json.dumps(results['images_results'], indent=2, ensure_ascii=False))
+
+    return render_template('index.html',carData=carData,form=form,model=model,images=images)
 
 @app.route("/hello/")
 @app.route("/hello/<var>")
 def helloFlask(var="DefaultValue"):
-    return render_template('helloFromFlask.html',name=var)
+    return render_template('helloFromFlask.html',carData=var)
     
 
 
